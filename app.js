5f19b8aa0dfcf28e19a9d6abfc98ff7ea81c17a9
Vicente Cortes <vice_012@hotmail.com>
2022-06-22 08:30:44 -0500

    sacamos el texto de nodemon

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('/', (req, res)=> {
    console.log('peticion recibida')
 
-   res.send('<H1>!Hola mundo con Nodemon</H1>')
+   res.send('<H1>!Hola mundo</H1>')
 })
 
 app.listen(4000, () => {
