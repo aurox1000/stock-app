c5979b148c42c4974ac317a0c4f1edae2ff58219
Vicente Cortes <vice_012@hotmail.com>
2022-07-01 05:55:34 -0500

    conexion con MDB + HTML, CSS, JS

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
-require('dotenv').config()
+
 const express = require ('express')
+const mongoose = require('mongoose')
+const path = require('path')
+
+require('dotenv').config()
 
 const app = express()
 
-app.get('/', (req, res)=> {
-   console.log('peticion recibida')
 
-   res.send('<H1>!Hola mundo</H1>')
+mongoose
+.connect(`mongodb+srv://aurox1000:${process.env.MONGO_DB_PASS}@developmet.thdbjwm.mongodb.net/stock-app?retryWrites=true&w=majority`
+)
+.then((result)=> {
+app.listen(PORT, () => {
+        console.log(`servidor escuchado en el puerto ${PORT}`)
+
+      })
+        console.log('conexion exitosa a la BDD')})
+
+.catch((falla) => console.log(falla))
+
+
+const productSchema = mongoose.Schema({
+   name: {type: String, required:true},
+   price: Number,
+},
+{timestamps: true }
+)
+
+const Product = mongoose.model('Product', productSchema)
+
+app.use(express.json())
+
+
+
+app.post('/api/v1/products',  (req, res) => {
+ 
+  
+ const newProduct = new Product(req.body)
+
+ newProduct
+.save()
+.then(result => {
+  
+  res.status(201).json({ok: true})
+
 })
+.catch((falla) => console.log(falla))
+
+
+})
+
+app.use(express.static(path.join( __dirname, 'public')))
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
-    console.log(`servidor escuchado en el puerto ${PORT}`)
 
 
-})
